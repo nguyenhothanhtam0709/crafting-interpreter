a13325d375df844490ea5d344b5ae0cc7e95a265
tam.nguyen <tam.nguyen@gmail.com>
2025-09-04 13:57:31 +0700

    feat: add support for some operator

--- clox/value.h
+++ clox/value.h
@@ -3,10 +3,38 @@
 
 #include "common.h"
 
+typedef enum
+{
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER
+} ValueType;
+
 /**
  * This typedef abstracts how Lox values are concretely represented in C.
+ *
+ * @details tagged union
  */
-typedef double Value;
+typedef struct
+{
+    ValueType type;
+    union
+    {
+        bool boolean;
+        double number;
+    } as;
+} Value;
+
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 /**
  * Constant pool
@@ -24,5 +52,6 @@ void initValueArray(ValueArray *array);
 void writeValueArray(ValueArray *array, Value value);
 void freeValueArray(ValueArray *array);
 void printValue(Value value);
+bool valuesEqual(Value a, Value b);
 
 #endif
